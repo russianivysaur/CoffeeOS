0670e928467742e20cd7939ad85d5f26b1b13704
Ankit <thakurankit036@gmail.com>
2024-12-24 22:55:16 +0530

    reading fats

--- fs/fat16/fat16.c
+++ fs/fat16/fat16.c
@@ -3,3 +3,24 @@
 //
 
 #include "fat16.h"
+#include "heap/kheap.h"
+
+struct BiosParameterBlock read_bpb(){
+  uint8_t* buffer = (uint8_t*)kzalloc(100);
+  lba_read(0,1,(uint16_t*)buffer);
+  struct BiosParameterBlock block;
+  block.oem_identifier = 0;
+  block.bytes_per_sector = 0;
+  block.sectors_per_clustor = 0;
+  block.reserved_sectors = 0;
+  block.fat_count = 0;
+  block.root_directory_count = 0;
+  block.total_sectors = 0;
+  block.media_descriptor_type = 0;
+  block.sectors_per_fat = 0;
+  block.sectors_per_track = 0;
+  block.number_of_heads = 0;
+  block.number_of_hidden_sectors = 0;
+  block.large_sector_count = 0;
+  return block;
+}
--- fs/fat16/fat16.h
+++ fs/fat16/fat16.h
@@ -1,8 +1,25 @@
 //
 // Created by bulbasaur on 24/12/24.
 //
-
+#include <stdint.h>
+#include "drivers/ata/ata.h"
 #ifndef FAT16_H
 #define FAT16_H
+
+struct BiosParameterBlock{
+  uint64_t oem_identifier;
+  uint16_t bytes_per_sector;
+  uint8_t sectors_per_clustor;
+  uint16_t reserved_sectors;
+  uint8_t fat_count;
+  uint16_t root_directory_count;
+  uint16_t total_sectors;
+  uint8_t media_descriptor_type;
+  uint16_t sectors_per_fat;
+  uint16_t sectors_per_track;
+  uint16_t number_of_heads;
+  uint32_t number_of_hidden_sectors;
+  uint32_t large_sector_count;
+};
 
 #endif //FAT16_H
